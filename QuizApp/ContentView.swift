769e607cb089711159480bd3541550f5f5facbcf
Beth-A <elizabeth78@me.com>
2020-07-31 09:12:14 -0400

    Initial Commit

---
+++ QuizApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuizApp
+//
+//  Created by Mary Arnold on 7/31/20.
+//  Copyright © 2020 Beth Arnold. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
